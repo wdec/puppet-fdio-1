bab603ae4fcdfa9668eb9c7f6b82fcb34c06d169
Wojciech Dec <wdec@cisco.com>
2018-01-25 22:25:44 +0100

    Remove FDIO VPP Repo initialization
    Change-Id: I1610062ebb0b7dc772987d155aad54038c016702
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,24 +3,24 @@
 # Manages the installation of fdio.
 #
 class fdio::install {
-  $base_url = $fdio::repo_branch ? {
-    'release' => 'https://nexus.fd.io/content/repositories/fd.io.centos7/',
-    'master'  => 'https://nexus.fd.io/content/repositories/fd.io.master.centos7/',
-    default   => "https://nexus.fd.io/content/repositories/fd.io.${fdio::repo_branch}.centos7/",
-  }
+#  $base_url = $fdio::repo_branch ? {
+#    'release' => 'https://nexus.fd.io/content/repositories/fd.io.centos7/',
+#    'master'  => 'https://nexus.fd.io/content/repositories/fd.io.master.centos7/',
+#    default   => "https://nexus.fd.io/content/repositories/fd.io.${fdio::repo_branch}.centos7/",
+#  }
 
   # Add fdio's Yum repository
-  yumrepo { "fdio-${fdio::repo_branch}":
-    baseurl  => $base_url,
-    descr    => "FD.io ${fdio::repo_branch} packages",
-    enabled  => 1,
-    gpgcheck => 0,
-  }
+#  yumrepo { "fdio-${fdio::repo_branch}":
+#    baseurl  => $base_url,
+#    descr    => "FD.io ${fdio::repo_branch} packages",
+#    enabled  => 1,
+#    gpgcheck => 0,
+#  }
 
   # Install the VPP RPM
   package { 'vpp':
     ensure  => present,
-    require => Yumrepo["fdio-${fdio::repo_branch}"],
+#    require => Yumrepo["fdio-${fdio::repo_branch}"],
   }
 
   if $fdio::vpp_dpdk_support {
