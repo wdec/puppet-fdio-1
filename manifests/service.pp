40a373cf07dd342ffb5dd325d4d50e7317b46ab7
Wojciech Dec <wdec@cisco.com>
2018-03-28 01:19:48 +0200

    Subscribe vpp service to vpp package changes
    Change-Id: I5b767d1fb735c054f410827fdbb8ddbd588a73c0
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,8 +3,26 @@
 # Configure and start VPP service.
 #
 class fdio::service {
-  service { 'vpp' :
-    ensure => running,
-    enable => true,
+  service { 'vpp':
+    ensure    => running,
+    enable    => true,
+    subscribe => Package['vpp'],
+  }
+
+  # Mop up vpp hugepages and systemd config, until it's fixed by RPM package
+  file { ['/etc/sysctl.d/80-vpp.conf', '/etc/sysctl.d/81-hugepages.conf']:
+    ensure    => absent,
+    subscribe => Package['vpp'],
+  }
+
+  exec { 'Reload sysctl config':
+    command     => 'sysctl --system',
+    path        => [ '/usr/sbin', '/sbin', '/usr/bin', '/bin' ],
+    refreshonly => true,
+    subscribe   => [
+      File['/etc/sysctl.d/80-vpp.conf'],
+      File['/etc/sysctl.d/81-hugepages.conf'],
+    ]
   }
 }
+
